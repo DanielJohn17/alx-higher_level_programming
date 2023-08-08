e50642cce0b97a2cba5fe2d71619bdfdefb8d24d
DanielJohn17 <danielyohannes38@gmail.com>
2023-08-08 17:53:49 +0300

    ascii code

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,8 +9,8 @@ else:
     ls_num = number % -10
 
 if ls_num > 5:
-    print(f"{string} {number} is {ls_num} and greater than 5")
+    print(f"{string} {number} is {ls_num} and is greater than 5")
 elif ls_num < 6 and ls_num != 0:
-    print(f"{string} {number} is {ls_num} and less than 6 and not 0")
+    print(f"{string} {number} is {ls_num} and is less than 6 and not 0")
 else:
     print(f"{string} {number} is {ls_num} and is 0")
