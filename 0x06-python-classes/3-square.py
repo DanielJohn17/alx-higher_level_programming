ff24d868147974555c7e9929081ad482eeb0f136
DanielJohn17 <danielyohannes38@gmial.com>
2023-08-29 21:43:35 +0300

    Classes and Objects

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Square module"""
+"""Square module."""
 
 
 class Square:
@@ -10,8 +10,8 @@ class Square:
             size: length of a side of a square.
 
         Raises:
-            TypeError: If size is not an intiger
-            ValueError: If size is less than 0
+            TypeError: If size is not an intiger.
+            ValueError: If size is less than 0.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
